042c7d9906fb77936a9baff1edb93e1f769c5ff0
logodev <pumpgem@gmail.com>
2024-07-18 14:52:59 +0300

    Добавили занятия

---
+++ lesson_1/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Console.WriteLine("Hello world!");   
+
+using System.Reflection.Metadata; 
+
+int numbers = 100;
+Console.WriteLine(numbers);
+
+double d = 100.5555555;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
+
+
+
+
+
+
---
+++ lesson_1/task_3/SecondProgram.cs
@@ -0,0 +1,40 @@
+int a = 15;
+int b = 115;
+int c = 1234;
+int d = 111;
+int e = 124;
+int max = a 
+
+if (b > max)
+{
+    max = b
+}
+else
+{
+   max = a
+}
+if (c > max)
+{
+    max = c
+}
+else
+{
+   max = a
+}
+if (d > max)
+{
+    max = d
+}
+else
+{
+   max = a
+}
+if (e > max)
+{
+    max = e
+}
+else
+{
+   max = a
+}
+Console.WriteLine(max)
